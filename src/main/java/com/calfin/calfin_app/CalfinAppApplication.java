264efe3baf97a95350ac743f686b5fea193d9a13
John <u202117377@upc.edu.pe>
2025-06-06 23:21:40 -0500

    chore: initial commit.

---
+++ src/main/java/com/calfin/calfin_app/CalfinAppApplication.java
@@ -0,0 +1,13 @@
+package com.calfin.calfin_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalfinAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalfinAppApplication.class, args);
+	}
+
+}
